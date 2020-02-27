3b9532228853e8c5b23c968d61f8be08085215a5
Zach Holland <zack09holland@gmail.com>
2020-02-26 21:12:55 -0800

    working out kinks with the new api access

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -146,19 +146,15 @@ module.exports = function(app) {
       favoriteDestination: req.body.favoriteDestination,
       UserId: req.user.id
     };
-    if(db.Favorites.findOne({ where: {favoriteTitle: req.body.favoriteTitle}})) {
-      void(0)
-    }else{
-      db.Favorites.create(object).then(function(data) {
-        res.json(data);
-      });
-    }   
-    // db.favorites.findorCreate({object}).spread(function(db, created) {
-    //   console.log(db.get({
-    //     plain: true
-    //   }))
-    //   console.log(created)
-    // })
+
+    // db.Favorites.create(object).then(function(data) {
+    //   res.json(data);
+    // });
+
+    db.favorites.findorCreate({object}).then(function(data) {
+      res.json(data)
+      console.log(data)
+    });
   });
   // Get all examples
   // app.get("/api/examples", function(req, res) {

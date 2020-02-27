71fc701167b6e7f323cebd03b511cf1d7f0190a8
Zach Holland <zack09holland@gmail.com>
2020-02-26 21:48:48 -0800

    working out kinks with the new api access

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -151,7 +151,7 @@ module.exports = function(app) {
     //   res.json(data);
     // });
 
-    db.Favorites.findorCreate({object}).then(function(data) {
+    db.Favorites.findOrCreate({object}).then(function(data) {
       res.json(data)
       console.log(data)
     });

9dac3989ccacb510c0c3f0fdb994f2059ebe3997
Zach Holland <zack09holland@gmail.com>
2020-02-29 19:14:04 -0800

    updating database and configuring favorites section

--- models/favorites.js
+++ models/favorites.js
@@ -12,7 +12,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: true
     },
-    // favoriteEventDate: DataTypes.DATEONLY,
+    eventDate: DataTypes.DATEONLY,
     favoriteStartDate: DataTypes.DATEONLY,
     favoriteEndDate: DataTypes.DATEONLY,
     favoriteDestination: DataTypes.STRING,

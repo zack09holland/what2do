fb196ee6e7d9ed63db0a0ea3a72ebbfd6ac54e69
Zach Holland <zack09holland@gmail.com>
2020-03-02 20:29:11 -0800

    updating database and configuring favorites section

--- public/js/favorites.js
+++ public/js/favorites.js
@@ -4,9 +4,16 @@ document.getElementById("goBack").addEventListener("click", function(event) {
 });
 
 $(document).ready(function() {
+  
   $.getJSON("/api/favorites").then(function(data) {
+    var container = $('.favorites')
+    var template = $('#tweets-template').html()
+
+    var templateHandlebars = Handlebars.compile(template);
+                    container.append(templateHandlebars(data));
+
     console.log(data);
-    console.log(data.results);
+    console.log(data[0].favoriteTitle);
   //   var dbFavs = $.map(data.results, function(tweet) {
   //     return {
   //         author: tweet.from_user,
